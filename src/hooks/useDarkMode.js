f83ace622a3788fee75748075397ab939c92a10a
Michael Perez <mperez6473@gmail.com>
2020-05-06 14:02:14 -0500

    Added the new hook to Navbar but it returned an error.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,6 +1,13 @@
 import React, { useEffect } from 'react';
 
-import useLocalStorage from './useLocalStorage';
-const useDarkMode = () => {
-    useLocalStorage('key');
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = () => {
+    const [someValue, setSomeValue] = useLocalStorage('key');
+    useEffect(() => {
+        if (useLocalStorage) {
+            document.body.classList.append('dark-mode');
+        } else document.body.classList.remove('dark-mode');
+    }, [useLocalStorage]);
+    return;
 };
